5861d7d4a38f6419f99a1475c9cd7212974e1fc6
Yanyiyi <Chunyan.liu@yannest.net>
2018-02-09 00:45:18 +0800

    add animation
    add animation

--- js/main.js
+++ js/main.js
@@ -7,5 +7,24 @@ console.log($(this).attr("href"));
 $(this).attr("href",$(this).attr("href")+"?"+n);
 console.log($(this).attr("href"));
 }); 
+   
+    let navbar = document.getElementsByClassName("navBar")[0];
+
+    
+    let action = document.getElementById("firstSec");
+        let offset = 0;
+        window.addEventListener("scroll", () => {
+            let bottom = action.getBoundingClientRect().bottom;
+            if ( bottom < action.offsetHeight - offset || window.innerWidth <= 768 )
+               {         navbar.style.animationDuration = "0.5s";
+        navbar.style.animationFillMode = "forwards";
+                   
+                   navbar.style.animationName = "backgroundFadeIn";
+                
+                }
+            
+            else navbar.style.animationName = "backgroundFadeOut";
+        });
+       
     
 });
