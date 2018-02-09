6ef0375e0db84053dfc50165fd0d7f92bb5e4571
Yanyiyi <Chunyan.liu@yannest.net>
2018-02-09 14:57:03 +0800

    motion Nav
    motion Nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     
-    $("link").each(function(){
+$("link").each(function(){
 var d = new Date();
 var n = d.getTime();
 console.log($(this).attr("href"));
@@ -9,20 +9,16 @@ console.log($(this).attr("href"));
 }); 
    
     let navbar = document.getElementsByClassName("navBar")[0];
-
-    
-    let action = document.getElementById("firstSec");
-        let offset = 0;
+    let action = document.getElementsByClassName("firstSec")[0];
+    let offset = 0;
         window.addEventListener("scroll", () => {
             let bottom = action.getBoundingClientRect().bottom;
             if ( bottom < action.offsetHeight - offset || window.innerWidth <= 768 )
-               {         navbar.style.animationDuration = "0.5s";
-        navbar.style.animationFillMode = "forwards";
-                   
-                   navbar.style.animationName = "backgroundFadeIn";
-                
+               {
+                navbar.style.animationDuration = "0.5s";
+                navbar.style.animationFillMode = "forwards";
+                navbar.style.animationName = "backgroundFadeIn";
                 }
-            
             else navbar.style.animationName = "backgroundFadeOut";
         });
        
